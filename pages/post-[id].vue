341c1022f4183faa02e2c53db7ffe186c7a8f926
hiren100 <hirenramoliya95@gmail.com>
2022-10-06 16:48:59 +0530

    nuxt 3 post listing and post detail page

---
+++ pages/post-[id].vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div>
+    <NuxtLink to="/features">
+      <h1 class="m-4 hover:underline">Back to Home</h1>
+    </NuxtLink>
+    <article class="m-4 md:w-1/2 lg:w-1/3">
+      <h1 class="mb-2 capitalize text-2xl font-semibold">{{ post.title }}</h1>
+      <p>{{ post.body }}</p>
+    </article>
+  </div>
+</template>
+
+<script setup>
+  const route = useRoute()
+  const { data: post } = await useFetch(`https://jsonplaceholder.typicode.com/posts/${route.params.id}`, { pick: ['title', 'body'] })
+</script>
